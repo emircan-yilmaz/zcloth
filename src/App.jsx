f02163ba6da730436bdb14e231baa2291b5ca3a3
emircan-yilmaz <emircany248@gmail.com>
2022-06-03 16:10:13 +0300

    Checkout page is done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./routes/home/home";
 import Navigation from "./routes/navigation/navigation";
 import Auth from "./routes/authentication/authentication";
 import Shop from "./routes/shop/shop";
+import Checkout from "./routes/checkout/checkout";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
         <Route path="auth" element={<Auth />} />
+        <Route path="checkout" element={<Checkout />} />
       </Route>
     </Routes>
   );
